f87b07e4827e35162b6c07bc3afa45e2c60ea9d5
Danielle Ismerim <danismerim@gmail.com>
2018-07-27 12:22:58 +0200

    adapting PHP7

--- lib/iterators/folders_iterator.php
+++ lib/iterators/folders_iterator.php
@@ -4,7 +4,8 @@ namespace symphony\extensions\static_asset_versioning;
 
 class FoldersIterator extends \AppendIterator
 {
-    public function append(FileFilterIterator $iterator)
+
+	public function append ( \Iterator $iterator )
     {
         parent::append($iterator);
     }
